2685636cf6feca9a0fdd9cb7051de7fe1c36c643
Richard Sprague <richardsprague@gmail.com>
2023-05-28 14:07:44 -0700

    handles embedded links for individual notes.

--- jopchop.py
+++ jopchop.py
@@ -1,31 +1,72 @@
 import os
 import shutil
 import sys
+import re
+import urllib.parse
+
 from joplinexport import joplinexport
 
 # Define the directory where you want to download the notes
 save_path = 'downloads'
+resource_id_to_file_path = {}
+note_id_to_title = {} # Initialize an empty dictionary outside save_note_to_file
 
+
+
+def replace_links_with_filenames(text):
+    pattern = r'(!?)\[(.*?)\]\(:/([a-fA-F0-9]+)\)'
+    def repl(matchobj):
+        is_image = matchobj.group(1)  # This will be '!' for images, and '' for links
+        title = matchobj.group(2)
+        id = matchobj.group(3)
+        # Check if id is a resource id or note id
+        if id in resource_id_to_file_path:
+            filename = resource_id_to_file_path[id]
+            return f'{is_image}[{title}]({filename})' 
+        elif id in note_id_to_title:
+            note_title = note_id_to_title[id]
+            note_title_encoded = urllib.parse.quote(note_title) + '.md'
+            return f'[{title}]({note_title_encoded})'
+        else:
+            return matchobj.group(0)  # If id not found, return the original link
+    return re.sub(pattern, repl, text)
 
 def save_note_to_file(note_title, note_body, note_id):
-    filename = os.path.join(save_path, f"{note_title.replace('/', '-')}.md")
-    with open(filename, 'w', encoding='utf-8') as f:
-        if note_body:
-            f.write(note_body)
-        else:
-            print(f"Note {note_id} does not have a body")
 
     resources = joplinexport.get_note_resources(note_id)
     for resource in resources['items']:
-        resource_data = joplinexport.download_resource(resource['id'])
-        if resource_data:
-            resource_filename = os.path.join(save_path, resource['title'].replace('/', '-'))
-            with open(resource_filename, 'wb') as f:
-                f.write(resource_data.data)
+        filename = resource['title'].replace('/', '-')
+        resource_filepath = os.path.join(save_path, filename)
+        with open(resource_filepath, 'wb') as f:
+            resource_data = joplinexport.download_resource(resource['id'])
+            shutil.copyfileobj(resource_data, f)
+        # Add the resource filename to the dictionary that maps resource IDs to filenames
+        resource_id_to_file_path[resource['id']] = filename
+
+    note_filename = os.path.join(save_path, f"{note_title.replace('/', '-')}.md")
+
+    note_id_to_title[note_id] = note_title
+
+    with open(note_filename, 'w', encoding='utf-8') as f:
+        # Replace the resource links with markdown links to the resource files before writing the body to the file
+        note_body = replace_links_with_filenames(note_body)
+        f.write(note_body)
+
+
+
+ 
 
 
 def export_notebook(notebook):
     notes = joplinexport.get_notes_from_notebook(notebook['id'])
+
+    # FIRST PASS
+    for note in notes:
+        note_title = note['title']
+        note_id = note['id']
+        note_id_to_title[note_id] = note_title  # Populate note_id_to_title
+    
+
     for note in notes:
         note_title, note_body = joplinexport.get_note_details(note['id'])
         save_note_to_file(note_title, note_body, note['id'])
