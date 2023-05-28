936b527e3f3f63f868ef44e7894558da9ccb5596
Richard Sprague <richardsprague@gmail.com>
2023-05-28 14:07:44 -0700

    handles embedded links for individual notes.

--- jopchop.py
+++ jopchop.py
@@ -1,27 +1,44 @@
 import os
 import shutil
 import sys
+import re
 from joplinexport import joplinexport
 
 # Define the directory where you want to download the notes
 save_path = 'downloads'
+resource_id_to_file_path = {}
+
+def replace_links_with_filenames(text):
+    pattern = r'\!\[([^\]]*)\]\(:/([a-z0-9]+)\)'
+    def repl(matchobj):
+        title = matchobj.group(1)
+        resource_id = matchobj.group(2)
+        filename = resource_id_to_file_path.get(resource_id, '')
+        return f'![{title}]({filename})'
+    return re.sub(pattern, repl, text)
 
 
 def save_note_to_file(note_title, note_body, note_id):
-    filename = os.path.join(save_path, f"{note_title.replace('/', '-')}.md")
-    with open(filename, 'w', encoding='utf-8') as f:
-        if note_body:
-            f.write(note_body)
-        else:
-            print(f"Note {note_id} does not have a body")
 
     resources = joplinexport.get_note_resources(note_id)
     for resource in resources['items']:
-        resource_data = joplinexport.download_resource(resource['id'])
-        if resource_data:
-            resource_filename = os.path.join(save_path, resource['title'].replace('/', '-'))
-            with open(resource_filename, 'wb') as f:
-                f.write(resource_data.data)
+        filename = resource['title'].replace('/', '-')
+        resource_filepath = os.path.join(save_path, filename)
+        with open(resource_filepath, 'wb') as f:
+            resource_data = joplinexport.download_resource(resource['id'])
+            shutil.copyfileobj(resource_data, f)
+        # Add the resource filename to the dictionary that maps resource IDs to filenames
+        resource_id_to_file_path[resource['id']] = filename
+
+    note_filename = os.path.join(save_path, f"{note_title.replace('/', '-')}.md")
+    with open(note_filename, 'w', encoding='utf-8') as f:
+        # Replace the resource links with markdown links to the resource files before writing the body to the file
+        note_body = replace_links_with_filenames(note_body)
+        f.write(note_body)
+
+
+
+ 
 
 
 def export_notebook(notebook):
